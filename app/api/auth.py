f2fff984ca27545aad182e168f23374e29e720d5
jimocool2 <muzan.jure@gmail.com>
2026-01-06 12:10:58 +0100

    fix indentation

--- app/api/auth.py
+++ app/api/auth.py
@@ -259,6 +259,7 @@ def current_user():
                   type: string
       401:
         description: Unauthorized - token invalid or expired
+        schema:
           type: object
           properties:
             error:
